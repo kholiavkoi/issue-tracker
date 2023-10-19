d8246b274f2814a6790b88befeb74dcd4a0c65ea
kholiavkoi <76649658+kholiavkoi@users.noreply.github.com>
2023-10-19 22:32:45 +0300

    create issue model

---
+++ app/NavBar.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="mb-5 flex h-14 items-center space-x-6 border-b px-5">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "transition-colors hover:text-zinc-800": true,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
